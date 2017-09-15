7697f6b34492663f6c9aca942cc5e7e9628d3cd3
raop <raop@users.noreply.github.com>
2017-09-15 19:13:11 -0400

    Added Binary Search

---
+++ src/com/algos/BinarySearch.java
@@ -0,0 +1,29 @@
+package com.algos;
+
+public class BinarySearch {
+    private static int DEFAULT = 1;
+    private int searchValue = DEFAULT;
+
+    public boolean search(Integer[] arr, int start, int end){
+        if (start >= end){
+            return false;
+        }
+
+        int mid = (start + end) / 2;
+        if (arr[mid] == searchValue) {
+            return true;
+        } else if (searchValue < arr[mid]) {
+            return search(arr, start, mid);
+        } else {
+            return search(arr, mid + 1, end);
+        }
+    }
+
+    public void setSearchValue(int val){
+        searchValue = val;
+    }
+
+    public int getSearchValue(){
+        return searchValue;
+    }
+}
--- src/com/algos/Main.java
+++ src/com/algos/Main.java
@@ -3,14 +3,14 @@ package com.algos;
 import com.helpers.Helpers;
 
 public class Main {
-    private static int SIZE = 99;
+    private static int SIZE = 200;
 
     public static void main(String[] args){
         Main x = new Main();
-        x.sort();
+        x.search();
     }
 
-    private void sort(){
+    private Integer[] sort(){
         System.out.println("Merge Sort");
 
         Integer arr[] = Helpers.getRandomArray(SIZE);
@@ -22,5 +22,18 @@ public class Main {
 
         System.out.println("After:");
         Helpers.printArray(arr);
+
+        return arr;
+    }
+
+    private void search() {
+        System.out.println("Binary search");
+
+        Integer[] arr = sort();
+
+        BinarySearch b = new BinarySearch();
+        b.setSearchValue(79);
+        boolean found = b.search(arr, 0, SIZE -1);
+        System.out.println(b.getSearchValue() + " was " + (found ? "" : "not ") + "found in the array");
     }
 }
